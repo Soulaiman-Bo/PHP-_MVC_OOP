489d14b0dce731959739fa928d2c2da1cd839c23
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2023-12-22 11:11:02 +0100

    I added delete method to The team Controller that serves the view of delete. and I added The deleteaction method that performs the deletion.

--- controller/team.php
+++ controller/team.php
@@ -13,7 +13,6 @@ class Team extends Controller
 		$view = $this->getView($viewmodel->Index(), false);
 		require_once "$view";
 	}
-
 	protected function update()
 	{
 		$id = $_GET['id'];
@@ -21,13 +20,17 @@ class Team extends Controller
 		if ($id !== "") {
 			$viewmodel = new TeamModel();
 			$rows = $viewmodel->selectSingleRecords("teams", "*", "team_id = $id");
-			$view = $this->getView($viewmodel->Index(), false);
-			require_once "$view";
-		}else {
+
+			if ($rows) {
+				$view = $this->getView($viewmodel->Index(), false);
+				require_once "$view";
+			}else {
+				echo "<h1>ERROR 404: Bad Request</h1>";
+			}
+		} else {
 			echo '<h1>ERROR 404: Page Not Found</h1>';
 		}
 	}
-
 	protected function add()
 	{
 
@@ -35,12 +38,20 @@ class Team extends Controller
 		$view = $this->getView($viewmodel->Index(), false);
 		require_once "$view";
 	}
-
 	protected function delete()
 	{
-		$viewmodel = new TeamModel();
-		$this->getView($viewmodel->Index(), false);
+		$id = $_GET['id'];
+
+		if ($id !== "") {
+			$teammodel = new TeamModel();
+			$row = $teammodel->selectSingleRecords("teams", "*", "team_id = $id");
+			$view = $this->getView($teammodel->Index(), false);
+			require_once "$view";
+		} else {
+			echo '<h1>ERROR 404: Bad Request</h1>';
+		}
 	}
+
 
 	protected function addaction()
 	{
@@ -79,8 +90,6 @@ class Team extends Controller
 
 		$id = $_POST['team_id'];
 
-
-		
 		$teamfields = array(
 			'team_name' => $team_name,
 			'founded_year' => $founded_year,
@@ -102,6 +111,25 @@ class Team extends Controller
 			echo json_encode([
 				"message" => $message,
 				"Id" => $insertedId
+			]);
+		}
+	}
+	protected function deleteaction()
+	{
+		extract($_POST);
+
+		$viewmodel = new TeamModel();
+
+		$id = $_POST['team_id'];
+
+		$result = $viewmodel->deleteRecord("teams", "team_id", $id);
+
+		if ($result) {
+			$message = "Team deleted successfully!";
+			http_response_code(200);
+			echo json_encode([
+				"message" => $message,
+				"Id" => $result
 			]);
 		}
 	}
