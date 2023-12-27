539d61d8e8d95c10a9b59d4215a954a48f19cfed
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2023-12-27 11:46:31 +0100

    i added validation class

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once "config.php";
 require_once "classes/Router.php";
 require_once "classes/Controller.php";
 require_once "classes/Model.php";
+require_once "classes/Validation.php.php";
+
 
 
 $router = new Router($_GET); // Get all url parameters
